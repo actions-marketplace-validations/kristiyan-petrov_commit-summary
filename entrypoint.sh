df491540f3cdd026ab00b6fd302586926d800443
kristiyan-petrov <136827928+kristiyan-petrov@users.noreply.github.com>
2024-04-24 17:56:58 +0200

    chore: update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ new_tag=$1
 previous_tag=$(git tag --sort=-creatordate | sed -n 2p)
 commit_summary="$(git log --oneline --pretty=tformat:"%h %s" $previous_tag..$new_tag)"
 commit_summary="${commit_summary//$'\n'/'%0A'}"
-echo ::set-output name=summary::$commit_summary
+echo "${summary}=${commit_summary}" >> $GITHUB_OUTPUT
